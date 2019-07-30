9a0e205701ef6eb25ae6865c11e9457f34fe5118
Yurii Holiuk <bionicuss@gmail.com>
2019-07-30 14:46:26 +0300

    solution

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-console.log('hello mate academy!');
+function main() {
+  document.querySelector('.buttons').addEventListener('click', ({ target }) => {
+    document.documentElement.style
+      .setProperty('--text-color', `var(--color-${target.dataset.color})`);
+  });
+}
+
+window.addEventListener('load', main);
